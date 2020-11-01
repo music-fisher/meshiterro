b8ca3c18cf5916b619b9e187ad7f664d887ac5cb
EC2 Default User <ec2-user@ip-172-31-21-33.ec2.internal>
2020-11-01 04:19:03 +0000

    [Finish]4-5投稿削除機能、マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/show'は重複避けるため
   root 'post_images#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :post_images,only: [:new,:create,:index,:show]do
+  resources :post_images,only: [:new,:create,:index,:show,:destroy]do
     resource :favorites,only:[:create,:destroy]
     resources :post_comments,only: [:create,:destroy]
   end
+  resources :users, only: [:show]
 end
